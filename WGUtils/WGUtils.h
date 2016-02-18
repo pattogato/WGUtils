2a635f93a6145a08e363b3a779fca7ed6923bf9d
Bence Pattogato <pattogatobence@gmail.com>
2016-02-18 18:31:12 +0100

    Initial Commit

---
+++ WGUtils/WGUtils.h
@@ -0,0 +1,19 @@
+//
+//  WGUtils.h
+//  WGUtils
+//
+//  Created by Bence Pattogato on 18/02/16.
+//  Copyright © 2016 Wintergarden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WGUtils.
+FOUNDATION_EXPORT double WGUtilsVersionNumber;
+
+//! Project version string for WGUtils.
+FOUNDATION_EXPORT const unsigned char WGUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WGUtils/PublicHeader.h>
+
+
